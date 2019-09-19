213425de70bfc0b0b7127f5a37369da25e7844c5
Garrett Luu <garrettluu131@gmail.com>
2019-09-19 15:41:41 -0700

    Fixed indentation and showMarkers

--- app/components/MapContainer.js
+++ app/components/MapContainer.js
@@ -1,40 +1,40 @@
 import React, {Component} from 'react'
 //TODO Change react-native-maps because it is deprecated?
-import MapView, {Marker} from 'react-native-maps'
+import MapView from 'react-native-maps'
+
+const Marker = MapView.Marker;
 
 /*
     Wrapper class for react MapView component
- */
+    */
 export default class MapContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
+  onRegionChange(region) {
+    this.state = {region};
+  }
 
-    onRegionChange(region) {
-        this.state = {region};
-    }
+  showMarkers() {
+    const {places} = this.props;
+    return places.map( function (place, i) {
+      return <Marker
+        key={i}
+        coordinate={place.geometry.location}
+        title={place.name}
+      />
+    });
+  }
 
-    showMarkers() {
-        return this.props.places.map((place) => (
-            <Marker
-                coordinate={place.latlng}
-                title={place.title}
-            />
-        ));
-    }
-
-    render() {
-        const { region } = this.props;
-        const { style } = this.props;
-        return (
-            <MapView
-                style={style}
-                showsUserLocation
-                onRegionChange={this.onRegionChange}
-                region={region}
-            >
-                {this.showMarkers()}
-            </MapView>
-        );
-    }
+  render() {
+    const { region } = this.props;
+    const { style } = this.props;
+    return (
+      <MapView
+        style={style}
+        showsUserLocation
+        onRegionChange={this.onRegionChange}
+        region={region}
+      >
+        {this.showMarkers()}
+      </MapView>
+    );
+  }
 }
